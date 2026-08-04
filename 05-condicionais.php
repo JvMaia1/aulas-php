b2806c1d4b4694358e360c38568bb3f701389f3e
Joao Vitor Hélio Costa maia <vmals@hotmail.com>
2026-08-04 08:45:01 -0300

    Usando condicionais simples com diferentes sintaxes

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -8,6 +8,20 @@
 <body>
     <h1>Trabalhando com estruturas condicionais</h1>
     <hr>
+    <h2>Condicional simples: <code>if</code></h2>
+    <?php 
+    $numero = 50;
+    if($numero > 10){
+        echo "<p>$numero é maior que 10</p>";
+    }
     
+    if($numero > 10) echo "<p>$numero é maior que 10</p>";
+    
+    if($numero > 10):
+        echo "<p>$numero é maior que 10</p>"; 
+    endif;
+
+    ?>
+
 </body>
 </html>
