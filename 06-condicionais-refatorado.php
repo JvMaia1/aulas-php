1665125fec7332ada3591ad8cf79e0b5e320a40d
Joao Vitor Hélio Costa maia <vmals@hotmail.com>
2026-08-04 11:11:36 -0300

    Usando php isolado do html de maneira legivel

--- 06-condicionais-refatorado.php
+++ 06-condicionais-refatorado.php
@@ -32,75 +32,33 @@ if($numero > 50){
 }
 ?>
 
-    <?php
+<?php
     
     if($numero > 10) echo "<p>$numero é maior que 10</p>";
     
     if($numero > 10):
         echo "<p>$numero é maior que 10</p>"; 
     endif;
-    ?>
-    
+?>
+
     <h2>Condicional composta <code>If/else</code></h2>
-    
-    <?php 
+
+<?php 
     $produto = "Ultrabook";
     $qtdEmEstoque = 0;
     $qtdCritica = 5;
-    
-    
-    ?>
+?>
+
     <h3><?= $produto ?></h3>
     <p><b>Quantidade em estoque: </b><?= $qtdEmEstoque ?></p>
 
-    <?php 
-    if($qtdEmEstoque < $qtdCritica) {
-        echo "<p style='color: red;'>É necessario comprar/repor</p>";
-        //condicional aninhada
-        if($qtdEmEstoque  === 0) echo "<p><mark class='comprar'>🚨Urgente</mark></p>";
+    <?php if($qtdEmEstoque < $qtdCritica): ?> <p style='color: red;'>É necessario comprar/repor</p>
     
-    } else {
-        echo "<p>Estoque normal</p>";
-    }
+        <?php if($qtdEmEstoque  === 0): ?> <p><mark class='comprar'>🚨Urgente</mark></p>
 
-    ?>
-    
-    <hr>
-    <h2>Condicional encadeada</h2>
+    <?php endif; else: ?> <p>Estoque normal</p>;
 
-    <?php 
-    $excessoDeEstoque = $qtdEmEstoque > 100 ? "Sim" : "Não";
+    <?php endif; ?>
 
-    $idade = 15;
-
-    if($idade <=12){
-        $situacao = "criança";
-
-    } elseif($idade <= 17){
-        $situacao = "adolescente";
-
-    } elseif($idade <= 59){
-        $situacao = "adulto";
-
-    } else {
-        $situacao = "idoso";
-    }
-
-    ?>
-    
-    <p>O usuário tem <?= $idade ?> e é <?= $situacao ?></p>
-    <h2>Usando os comandos <code>switch/case/default/break</code></h2>
-
-    <?php 
-    // Prototipo chatbot
-    //opções: 1 (informaçoes), 2 (reclamação), 3 (elogio), x (invalida)
-    $opcao = 1; // input simulado
-    switch($opcao){
-        case 1: echo "<p>Legal, o que quer saber?</p>"; break;
-        case 2: echo "<p>Que pena, o que houve?</p>"; break;
-        case 3: echo "<p>Que bacana, pode falar!</p>"; break;
-        default: echo "<p>Não entendi... vou te encaminhar para um especialista</p>";
-    }
-    ?>
 </body>
 </html>
