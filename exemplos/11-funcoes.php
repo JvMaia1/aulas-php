7887c50e65d6c7d39aa88bf44f7cb761abcc8ee2
Joao Vitor Hélio Costa maia <vmals@hotmail.com>
2026-08-12 11:58:27 -0300

    exemplo funçoes (recap)

--- exemplos/11-funcoes.php
+++ exemplos/11-funcoes.php
@@ -30,9 +30,51 @@ function somar($valor1, $valor2){
             return "Olá, $mensagem $pessoa";
         }
         ?>
+    <div class="container bg-dark text-white rounded-3 p-2 px-4">
+    <code>
+        <pre>
+
+function exibirMensagem($mensagem, $pessoa = ''){
+    return "Olá, $mensagem $pessoa";
+}</pre>
+    </code>
+    </div>
+        
 
         <p>Saudação 1: <?= exibirMensagem('Bom dia') ?></p>
         <p>Saudação 1: <?= exibirMensagem('Bom dia', 'Fulano') ?></p>
+
+        <hr>
+
+        <h2>Função com indução de tipo de dados</h2>
+        <p>Nesta abordagem, definimos tipos de dados para os parãmetros e para o retorno da função</p>
+
+        <?php 
+        function verificarNegativo(int $valor):string{
+            if($valor < 0){
+                return 'é negativo';
+            } else {
+                return 'Não é negativo';
+            }
+        };
+        ?>
+        <div class="container bg-dark text-white rounded-3 p-2 px-4">
+            <code>
+                <pre>
+
+function verificarNegativo(int $valor):string{
+    if($valor < 0){
+        return 'é negativo';
+} else {
+    return 'Não é negativo';
+    }
+};</pre>
+            </code>
+        </div>
+        <div class="container">
+            <p class="border-3 border-dark">10 <?= verificarNegativo(10) ?></p>
+            <p class="border-3 border-dark">-10 <?= verificarNegativo(-10) ?></p>
+        </div>
 </div>
 </body>
 </html>
